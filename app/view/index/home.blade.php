45f950e921f8fab62498c47d997a15ad8771151c
huyong <huyong@xinkin.com>
2026-03-24 14:06:04 +0800

    page title update

--- app/view/index/home.blade.php
+++ app/view/index/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>批量上传</title>
+    <title>文件上传</title>
     <script>
         (function () {
             try {
